4e12a217e04dbf8c4c2f1997f157440449849cc7
Micaiah Obinwa <micaiahobinwa4data@gmail.com>
2024-07-04 18:10:55 +0100

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -22,6 +25,15 @@ class DataIngestion:
         try:
             df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset in csv')
+
+            # Calculate the 'average' column
+            df['average'] = (df['math_score'] + df['reading_score'] + df['writing_score']) / 3
+            logging.info("Calculated 'average' column")
+
+            # Save the updated DataFrame with 'average' column to raw data path
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Saved raw data with 'average' column")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
@@ -46,4 +58,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
